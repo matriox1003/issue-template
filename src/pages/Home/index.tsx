851c5b12adde7d61a77c34857021ae41fe929d19
jinhaojie <97108437@qq.com>
2023-05-31 14:35:52 +0800

    init

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,24 @@
+import Guide from '@/components/Guide';
+import { trim } from '@/utils/format';
+import { PageContainer } from '@ant-design/pro-components';
+import { useModel } from '@umijs/max';
+import { useEffect } from 'react';
+import styles from './index.less';
+
+const HomePage: React.FC = () => {
+  const { name } = useModel('global');
+
+  useEffect(() => {
+    throw new Error('test');
+  }, []);
+
+  return (
+    <PageContainer ghost>
+      <div className={styles.container}>
+        <Guide name={trim(name)} />
+      </div>
+    </PageContainer>
+  );
+};
+
+export default HomePage;
